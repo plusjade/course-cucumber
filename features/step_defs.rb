cee5bace10d478f8508ff97f4e3a8ff090ac504c
Jade Dominguez <plusjade@gmail.com>
2014-03-31 19:51:34 -0700

    Implement Comment class handle @mentions feature.
    We'll start adding the code that will actually implement the feature. Notice we add and call the very logical `comment.body` method to our Comment class and step definition. Even though there's no code, we've already designed a very reasonable API (application programming interface) to work with our library.

--- features/step_defs.rb
+++ features/step_defs.rb
@@ -1,5 +1,6 @@
-Given(/^I write a comment with body "(.*?)"$/) do |arg1|
-  pending # express the regexp above with the code you wish you had
+Given(/^I write a comment with body "(.*?)"$/) do |body|
+  comment = Comment.new(body)
+  puts comment.body
 end
 
 Then(/^the comment's mentions should include the user "(.*?)"$/) do |arg1|
