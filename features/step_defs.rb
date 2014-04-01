b4c174f8d8f11889891478bcd29b48bb904b2199
Jade Dominguez <plusjade@gmail.com>
2014-03-31 19:46:55 -0700

    Add step definition scaffolds generated by cucumber.
    These step definitions match the steps we wrote to test the @mentions feature in the previous commit.

---
+++ features/step_defs.rb
@@ -0,0 +1,7 @@
+Given(/^I write a comment with body "(.*?)"$/) do |arg1|
+  pending # express the regexp above with the code you wish you had
+end
+
+Then(/^the comment's mentions should include the user "(.*?)"$/) do |arg1|
+  pending # express the regexp above with the code you wish you had
+end
