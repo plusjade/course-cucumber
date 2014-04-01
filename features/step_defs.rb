f1669e406de570d62e92e3295470c647c17ab139
Jade Dominguez <plusjade@gmail.com>
2014-03-31 20:36:38 -0700

    Add new step definition for multiple @mentions scenario.

--- features/step_defs.rb
+++ features/step_defs.rb
@@ -6,3 +6,8 @@ end
 Then(/^the comment's mentions should include the user "(.*?)"$/) do |name|
   expect(@comment.mentions).to include(name)
 end
+
+Then(/^the comment's mentions should include the users "(.*?)" and "(.*?)"$/) do |name1, name2|
+  expect(@comment.mentions).to include(name1)
+  expect(@comment.mentions).to include(name2)
+end
