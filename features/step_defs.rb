6612f20bff153be4c565bb9518588c264a5f0450
Jade Dominguez <plusjade@gmail.com>
2014-03-31 20:03:12 -0700

    Store comment as instance method so it's accessible throughout the environment.
    All ruby code loaded into the cucumber environment is simply one big shared environment.

--- features/step_defs.rb
+++ features/step_defs.rb
@@ -1,8 +1,8 @@
 Given(/^I write a comment with body "(.*?)"$/) do |body|
-  comment = Comment.new(body)
-  puts comment.body
+  @comment = Comment.new(body)
+  puts @comment.body
 end
 
 Then(/^the comment's mentions should include the user "(.*?)"$/) do |arg1|
-  puts comment.mentions
+  puts @comment.mentions
 end
